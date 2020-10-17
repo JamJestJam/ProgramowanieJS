f2d385daa2c317ff96338b6921ff113633da57e6
JamJestJam <marekti012@gmail.com>
2020-10-17 15:03:58 +0200

    .

--- cw3/js.js
+++ cw3/js.js
@@ -1,4 +1,5 @@
-const table = {
+/* eslint-disable no-prototype-builtins */
+const tableSongs = {
     'KeyQ': 'boom.wav',
     'KeyW': 'clap.wav',
     'KeyE': 'hihat.wav',
@@ -9,13 +10,76 @@ const table = {
     'KeyF': 'tink.wav',
     'KeyG': 'tom.wav',
 };
-//const recordedSounds[];
-//let recordStartTime;
-document.addEventListener('keypress', logKey);
+const tableKeys = {
+    'Space': startStopRecord,
+    'Enter': playMusic
+};
+const playBTN = document.querySelector('#play');
+const recordBTN = document.querySelector('#record');
+const recordName = document.querySelector('#RecordName');
+const musicList = document.querySelector('#music');
 
-function logKey(e) {
-    if(table.hasOwnProperty(e.code)){
-        new Audio('./music/' + table[e.code]).play();
+document.addEventListener('keypress', keyPress);
+recordBTN.addEventListener('click', startStopRecord);
+recordName.addEventListener('keypress', stopPropagation);
+playBTN.addEventListener('click', playMusic);
+
+function keyPress(e) {
+    if (tableSongs.hasOwnProperty(e.code)) {
+        play(tableSongs[e.code]);
+
+        if (recordBTN.innerHTML == 'Stop') {
+            let music = musicList.options[musicList.options.length - 1];
+            music.arrayMusic.push({
+                'Date': Date.now() - music.startMusic,
+                'Song': tableSongs[e.code]
+            });
+        }
+    } else if (tableKeys.hasOwnProperty(e.code)) {
+        tableKeys[e.code]();
     }
-    
+}
+
+function startStopRecord() {
+    if (recordBTN.innerText == 'Record') {
+        let name = recordName.value;
+        if (name == '') {
+            alert('You have to name the recording');
+            return;
+        }
+
+        let option = new Option();
+        option.value = name;
+        option.text = name;
+        option.startMusic = Date.now();
+        option.arrayMusic = new Array();
+        musicList.options.add(option);
+
+        recordBTN.innerHTML = 'Stop';
+    } else {
+        recordBTN.innerHTML = 'Record';
+    }
+}
+
+function playMusic() {
+    let nr = musicList.options.selectedIndex;
+    let music = musicList.options[nr].arrayMusic;
+
+    if (nr == 0) {
+        alert('no track selected');
+        return;
+    }
+
+    music.forEach(element => {
+        setTimeout(() => play(element.Song), element.Date);
+    });
+}
+
+function play(musicName) {
+    let tmp = new Audio('./music/' + musicName);
+    tmp.play();
+}
+
+function stopPropagation(e) {
+    e.stopPropagation();
 }
