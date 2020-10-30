f1e326927d5b7f62c1e786d44df328714beaa460
JamJestJam <marekti012@gmail.com>
2020-10-30 17:13:18 +0100

    .

--- cw4/main.js
+++ cw4/main.js
@@ -1,22 +1,23 @@
 let notes = [];
 
-function getNotesFromLocalStorage(){
+function getNotesFromLocalStorage() {
     const LS_elements = JSON.parse(localStorage.getItem('Notes'));
     notes = LS_elements;
 }
 
-function setNotesToLocalStorage(){
+function setNotesToLocalStorage() {
     localStorage.setItem('Notes', JSON.stringify(notes));
 }
 
-function createNewNote(title, context, colour = '#cccccc', pinned = false, date = new Date()){
-    this.title = title;
-    this.context = context;
-    this.colour = colour;
-    this.pinned = pinned;
-    this.createDate = date;
+class createNewNote {
+    constructor(title, context, colour = '#cccccc', pinned = false, date = new Date()) {
+        this.title = title;
+        this.context = context;
+        this.colour = colour;
+        this.pinned = pinned;
+        this.createDate = date;
+    }
 }
 
 getNotesFromLocalStorage();
 
-console.log(new createNewNote('cos', 'tmp'));
